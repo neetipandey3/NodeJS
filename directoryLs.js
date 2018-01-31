2c9ab73d4b3e2590eb1ed0430eacda6f2e3535bc
neetipandey3 <31138437+neetipandey3@users.noreply.github.com>
2018-01-31 12:12:41 -0800

    Update directoryLs.js

--- directoryLs.js
+++ directoryLs.js
@@ -4,9 +4,6 @@ var mymodule = require('./exportModule.js')
 var myPath = process.argv[2]
 var myExtension = process.argv[3]
 
-
-
-
 mymodule(myPath, myExtension, function(err, filelist) {
     if (err){
         throw err
