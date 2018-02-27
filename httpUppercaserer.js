eb37a33b56c99bb44ce31191b2ad059ba2326d7e
neetipandey3 <31138437+neetipandey3@users.noreply.github.com>
2018-02-26 17:53:01 -0800

    Update httpUppercaserer.js
    Removed unwanted lines of code

--- httpUppercaserer.js
+++ httpUppercaserer.js
@@ -1,12 +1,11 @@
 var http = require('http') 
-var fs = require('fs') 
 var port = process.argv[2]
 var map = require('through2-map')
 
 
 var server = http.createServer(function (req, res){   
     /*
-    Nodeschool code: if (req.method !== 'POST') {
+    Nodeschool additional code: if (req.method !== 'POST') {
         return res.end('send me a POST\n')
     } 
     */
